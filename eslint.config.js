c209ca10a5e811cad00f945dc32508e08f3b401b
brandonperfetti <brandon@brandonperfetti.com>
2025-11-30 15:08:27 -0800

    Adopts Prettier, improves code consistency
    Integrates Prettier and Tailwind CSS Prettier plugin for code formatting.
    Standardizes quote usage and formatting across codebase for better readability and maintainability.
    Helps ensure consistent code style for future development.

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 // If you are developing a production application, we recommend updating the configuration to enable type-aware lint rules
 
 export default defineConfig([
-	globalIgnores(["dist"]),
+	globalIgnores(['dist']),
 	{
-		files: ["**/*.{ts,tsx}"],
+		files: ['**/*.{ts,tsx}'],
 		extends: [
 			js.configs.recommended,
 			//   tseslint.configs.recommended,
