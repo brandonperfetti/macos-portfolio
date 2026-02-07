25b857f210eb5602b88ad504ed4e7cadb2cadaab
brandonperfetti <brandon@brandonperfetti.com>
2026-02-07 15:34:41 -0800

    eslint: add __dirname for ESM and enable type-aware parserOptions

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,13 @@
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // If you are developing a production application, we recommend updating the configuration to enable type-aware lint rules
 
@@ -26,6 +30,10 @@ export default defineConfig([
 		languageOptions: {
 			ecmaVersion: 2020,
 			globals: globals.browser,
+			parserOptions: {
+				project: ['./tsconfig.app.json', './tsconfig.node.json'],
+				tsconfigRootDir: __dirname,
+			},
 		},
 	},
 ]);
